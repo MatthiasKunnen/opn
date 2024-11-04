5ad6a11f3493411fd9dc4b14cc0f45b5cae0be6e
Matthias Kunnen <matthias.kunnen@gmail.com>
2024-11-04 18:33:38 +0100

    Add completion for query --format flag

--- cmd/opn/query/query.go
+++ cmd/opn/query/query.go
@@ -3,6 +3,7 @@ package query
 import (
 	"github.com/spf13/cobra"
 	"github.com/thediveo/enumflag/v2"
+	"log"
 )
 
 type outputMode enumflag.Flag
@@ -46,11 +47,21 @@ func init() {
 		"Do not use the cache. Instead, all lookups are performed on the file system.",
 	)
 
+	formatFlag := enumflag.New(&format, "format", outputModeMap, enumflag.EnumCaseInsensitive)
+	const formatFlagName = "format"
 	QueryCmd.PersistentFlags().Var(
-		enumflag.New(&format, "format", outputModeMap, enumflag.EnumCaseInsensitive),
-		"format",
+		formatFlag,
+		formatFlagName,
 		`Sets the output format. Either json or verbose. The verbose output is not stable.
 If the result is to be processed by a script, use the json format.`)
+
+	err := formatFlag.RegisterCompletion(QueryCmd, formatFlagName, enumflag.Help[outputMode]{
+		outputJson:    "Stdout is JSON.",
+		outputVerbose: "Stdout is verbose text and not stable.",
+	})
+	if err != nil {
+		log.Printf("failed to register shell completion of query --format flag: %v\n", err)
+	}
 
 	// @todo Add compact option with separator. 0 => nul delimited, all the rest, simple
 	//   replacement.
